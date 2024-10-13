5c71155ae40504c59d777017ae8b3bb045407e18
Gayathri <143020568+Gayathri-K-S@users.noreply.github.com>
2024-10-13 19:28:51 +0530

    Update Preview.jsx

--- src/components/Home/Write/Preview.jsx
+++ src/components/Home/Write/Preview.jsx
@@ -1,59 +1,160 @@
-import React, { useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import React, { useEffect, useRef, useState } from "react";
+import { LiaTimesSolid } from "react-icons/lia";
 import ReactQuill from "react-quill";
-import 'react-quill/dist/quill.bubble.css';
-import Quill from 'quill';
-import AutoLinks from 'quill-auto-links';
-import Preview from "./Preview";
+import TagsInput from "react-tagsinput";
+import { toast } from "react-toastify";
+import { db, storage } from "../../../firebase/firebase";
 import { Blog } from "../../../Context/Context";
-// Assuming you added the CSS in Write.css
+import { useNavigate } from "react-router-dom";
 
-// Register the AutoLinks module with Quill
-Quill.register('modules/autoLinks', AutoLinks);
+const Preview = ({ setPublish, description, title }) => {
+  const imageRef = useRef(null);
+  const [imageUrl, setImageUrl] = useState("");
+  const [tags, setTags] = useState([]);
+  const [desc, setDesc] = useState("");
+  const { currentUser } = Blog();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
-const Write = () => {
-  const [description, setDescription] = useState("");
-  const [title, setTitle] = useState("");
-  const { publish, setPublish } = Blog();
+  const [preview, setPreview] = useState({
+    title: "",
+    photo: "",
+  });
 
-  const modules = {
-    toolbar: [
-      [{ 'link': 'link' }], // Add link button to toolbar
-    ],
-    autoLinks: true // Enable autoLinks
+  useEffect(() => {
+    if (title || description) {
+      setPreview({ ...preview, title: title });
+      setDesc(description);
+    } else {
+      setPreview({ ...preview, title: "" });
+      setDesc("");
+    }
+  }, [title, description]);
+
+  const handleClick = () => {
+    imageRef.current.click();
+  };
+
+  const handleSubmit = async () => {
+    setLoading(true);
+    try {
+      if (preview.title === "" || desc === "" || tags.length === 0) {
+        toast.error("All fields are required!!!");
+        return;
+      }
+
+      if (preview.title.length < 15) {
+        toast.error("Title must be at least 15 letters");
+        return;
+      }
+
+      const collections = collection(db, "posts");
+
+      let url;
+      if (imageUrl) {
+        const storageRef = ref(storage, `image/${preview.photo.name}`);
+        await uploadBytes(storageRef, preview?.photo);
+
+        url = await getDownloadURL(storageRef);
+      }
+
+      await addDoc(collections, {
+        userId: currentUser?.uid,
+        title: preview.title,
+        desc,
+        tags,
+        postImg: url || "",
+        created: Date.now(),
+        pageViews: 0,
+      });
+      toast.success("Post has been added");
+      navigate("/");
+      setPublish(false);
+      setPreview({
+        title: "",
+        photo: "",
+      });
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
-    <section className="w-[90%] md:w-[90%] lg:w-[60%] mx-auto py-[3rem]">
-      <input
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        type="text"
-        placeholder="Title"
-        className="text-4xl border rounded-md outline-none w-full font-semibold"
-      />
-
-      <ReactQuill
-        theme="bubble"
-        value={description}
-        onChange={setDescription}
-        modules={modules} // Add toolbar and auto-link detection
-        placeholder="Tell Your Story..."
-        className="write my-5" // Apply styles using this class
-      />
-
-      <div
-        className={`${
-          publish ? "visible opacity-100" : "invisible opacity-0"
-        } transition-all duration-200`}
-      >
-        <Preview
-          setPublish={setPublish}
-          description={description}
-          title={title}
-        />
+    <section className="absolute inset-0 bg-white z-30">
+      <div className="size my-[2rem]">
+        <span
+          onClick={() => setPublish(false)}
+          className="absolute right-[1rem] md:right-[5rem] top-[3rem] text-2xl cursor-pointer">
+          <LiaTimesSolid />
+        </span>
+        {/* Preview the text */}
+        <div className="mt-[8rem] flex flex-col md:flex-row gap-10">
+          <div className="flex-[1]">
+            <h3>Story Preview</h3>
+            <div
+              style={{ backgroundImage: `url(${imageUrl})` }}
+              onClick={handleClick}
+              className="w-full h-[200px] object-cover bg-gray-100 my-3 grid 
+                place-items-center cursor-pointer bg-cover bg-no-repeat">
+              {!imageUrl && "Add Image"}
+            </div>
+            <input
+              onChange={(e) => {
+                setImageUrl(URL.createObjectURL(e.target.files[0]));
+                setPreview({ ...preview, photo: e.target.files[0] });
+              }}
+              ref={imageRef}
+              type="file"
+              hidden
+            />
+            <input
+              type="text"
+              placeholder="Title"
+              className="outline-none w-full border-b border-gray-300 py-2"
+              value={preview.title}
+              onChange={(e) =>
+                setPreview({ ...preview, title: e.target.value })
+              }
+            />
+            <ReactQuill
+              theme="bubble"
+              value={desc}
+              onChange={setDesc}
+              placeholder="Tell Your Story..."
+              className="py-3 border-b border-gray-300"
+            />
+            <p className="text-gray-500 pt-4 text-sm">
+              <span className="font-bold">Note:</span> Changes here will affect
+              how your story appears in public places like Penit's homepage and
+              in subscribers’ inboxes — not the contents of the story itself.
+            </p>
+          </div>
+          <div className="flex-[1] flex flex-col gap-4 mb-5 md:mb-0">
+            <h3 className="text-2xl">
+              Publishing to:
+              <span className="font-bold capitalize">
+                {currentUser?.displayName?.split('-')[1]?.trim() || 'Default Name'}
+              </span>
+            </h3>
+            <p>
+              Add or change topics up to 5 so readers know what your story is
+              about.
+            </p>
+            <TagsInput value={tags} onChange={setTags} />
+            <button
+              onClick={handleSubmit}
+              className="btn !bg-green-800 !w-fit !text-white !rounded-full">
+              {loading ? "Submitting..." : "Publish Now"}
+            </button>
+          </div>
+        </div>
       </div>
     </section>
   );
 };
 
-export default Write;
+export default Preview;
