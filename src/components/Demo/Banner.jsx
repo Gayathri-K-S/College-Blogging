86648f1dbf263ffb13c112f14c7130a061282c83
Gayathri <143020568+Gayathri-K-S@users.noreply.github.com>
2024-10-27 21:48:39 +0530

    Update Banner.jsx

--- src/components/Demo/Banner.jsx
+++ src/components/Demo/Banner.jsx
@@ -1,8 +1,14 @@
 import React from "react";
-import Auth from "./Auth/Auth"
+
 const Banner = () => {
   return (
-    <div className="bg-banner border-b border-black">
+    <div
+      className="bg-center bg-cover h-[60vh] flex items-center justify-center"
+      style={{
+        backgroundImage: `url('psgimg.jpg')`,
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
       <div className="size py-[5rem] flex flex-col items-start gap-[1rem]">
         <h1 className="font-title text-[3rem] sm:text-[4rem] md:text-[6rem] font-normal">
           PenIt
@@ -11,7 +17,7 @@ const Banner = () => {
           Discover stories, thinking, and expertise from writers on any topic.
         </p>
         <button className="btn bg-black1 rounded-full text-white !text-[1.2rem] !px-6 !mt-[2.5rem]">
-          <Auth/>
+          Start reading
         </button>
       </div>
     </div>
