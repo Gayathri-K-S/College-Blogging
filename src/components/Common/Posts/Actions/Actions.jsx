5d2ea6f42f80f5e430bc3876d393e731addd3173
Gayathri <143020568+Gayathri-K-S@users.noreply.github.com>
2024-10-27 11:25:37 +0530

    Update Actions.jsx

--- src/components/Common/Posts/Actions/Actions.jsx
+++ src/components/Common/Posts/Actions/Actions.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { BsThreeDots } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
 import { Blog } from "../../../../Context/Context";
 import { deleteDoc, doc } from "firebase/firestore";
@@ -7,10 +8,15 @@ import { toast } from "react-toastify";
 
 const Actions = ({ postId, title, desc }) => {
   const { setUpdateData, currentUser } = Blog();
-  const navigate = useNavigate();
+  const [showDrop, setShowDrop] = useState(false);
+  const handleClick = () => {
+    setShowDrop(!showDrop);
+  };
+
+  const navigate = useNavigate(null);
 
   const handleEdit = () => {
-    navigate(`/editPost/${postId}`);
+    navigate(/editPost/${postId});
     setUpdateData({ title, description: desc });
   };
 
@@ -26,20 +32,38 @@ const Actions = ({ postId, title, desc }) => {
         "savedPost",
         postId
       );
-
       await deleteDoc(ref);
       await deleteDoc(likeRef);
       await deleteDoc(commentRef);
       await deleteDoc(savedPostRef);
 
-      toast.success("Post has been removed");
+      toast.success("post has been removed");
+      setShowDrop(false);
       navigate("/");
     } catch (error) {
-      toast.error(error.message);
+      toast.success(error.message);
     }
   };
-
-  return <div className="relative">{/* Actions without dropdown */}</div>;
+  return (
+    <div className="relative">
+      <button onClick={handleClick}>
+        <BsThreeDots className="text-2xl" />
+      </button>
+     
+    </div>
+  );
 };
 
 export default Actions;
+
+const Button = ({ click, title }) => {
+  return (
+    <button
+      onClick={click}
+      className={p-2 hover:bg-gray-100 hover:text-black/80 w-full text-sm text-left
+    ${title === "Delete Story" ? "text-red-600" : ""}
+    }>
+      {title}
+    </button>
+  );
+};
