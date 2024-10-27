4cd54b4c819121a01ceaec598733be44881ccef8
Gayathri <143020568+Gayathri-K-S@users.noreply.github.com>
2024-10-27 21:37:27 +0530

    Update Banner.jsx

--- src/components/Demo/Banner.jsx
+++ src/components/Demo/Banner.jsx
@@ -11,7 +11,7 @@ const Banner = () => {
           Discover stories, thinking, and expertise from writers on any topic.
         </p>
         <button className="btn bg-black1 rounded-full text-white !text-[1.2rem] !px-6 !mt-[2.5rem]">
-          <Auth.jsx/>
+          <Auth/>
         </button>
       </div>
     </div>
